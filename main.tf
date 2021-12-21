9806dfabf9ab618a76e2f7702218ba4241b50c89
gurusaligram <59478619+gurusaligram@users.noreply.github.com>
2021-12-21 19:16:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "azurerm" {
-        features {}
-        subscription_id      = "85d63697-9e8f-44fc-840e-c49c8f578545"
-        tenant_id            = "94655c80-98c5-402e-b2a2-ee58e8d2d75b"
-	client_id 	     = "d4dca534-7cef-4996-9abd-3ffd88b85b37"
-	client_secret        = 
-}
-
 resource "azurerm_resource_group" "example" {
   name     = "jenkins-demo-rsg"
   location = "East US"
