647b7828c99715dcf1d4e6275c2d5b0ac6bc1f41
gurusaligram <59478619+gurusaligram@users.noreply.github.com>
2021-12-21 19:16:29 +0530

    Update provider.tf

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "azurerm" {
+        features {}
+        subscription_id      = "85d63697-9e8f-44fc-840e-c49c8f578545"
+        tenant_id            = "94655c80-98c5-402e-b2a2-ee58e8d2d75b"
+}
