74f6c46cb48b2616e85dc01c862adadc15d0de81
gurusaligram <59478619+gurusaligram@users.noreply.github.com>
2021-12-22 10:46:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "example" {
   name     = "jenkins-demo-rsg"
   location = "East US"
 }
+
+
+resource "azurerm_resource_group" "example1" {
+  name     = "jenkins-demo-eu-rsg"
+  location = "UK South"
+}
+
