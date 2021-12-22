90c33f2aeaeef62bee9db3dbc4bba22c0d4b3045
gurusaligram <59478619+gurusaligram@users.noreply.github.com>
2021-12-22 10:35:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,6 +13,6 @@ provider "azurerm" {
   
   use_msi = true
   
- #       subscription_id      = "85d63697-9e8f-44fc-840e-c49c8f578545"
- #      tenant_id            = "94655c80-98c5-402e-b2a2-ee58e8d2d75b"
+  subscription_id      = "85d63697-9e8f-44fc-840e-c49c8f578545"
+  tenant_id            = "94655c80-98c5-402e-b2a2-ee58e8d2d75b"
 }
