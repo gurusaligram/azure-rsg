05afd9ab1a8fb3c1d7654cbc216dbc9952a2c158
gurusaligram <59478619+gurusaligram@users.noreply.github.com>
2021-12-22 10:30:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,6 +10,9 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-        subscription_id      = "85d63697-9e8f-44fc-840e-c49c8f578545"
-        tenant_id            = "94655c80-98c5-402e-b2a2-ee58e8d2d75b"
+  
+  use_msi = true
+  
+ #       subscription_id      = "85d63697-9e8f-44fc-840e-c49c8f578545"
+ #      tenant_id            = "94655c80-98c5-402e-b2a2-ee58e8d2d75b"
 }
