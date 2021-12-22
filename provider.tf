ce6d47326c4f94aecc4d45cde23e9ee88ffab285
gurusaligram <59478619+gurusaligram@users.noreply.github.com>
2021-12-22 09:55:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-        features {}
+  features {}
         subscription_id      = "85d63697-9e8f-44fc-840e-c49c8f578545"
         tenant_id            = "94655c80-98c5-402e-b2a2-ee58e8d2d75b"
 }
